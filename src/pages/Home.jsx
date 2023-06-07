878e856093f1e569a3f600b6eabf4c581e554e6b
maximilianopietkiewicz <maximilianopietkiewicz04@gmail.com>
2023-06-07 16:24:20 -0300

    comenzando la pagina de inicio...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { Navbar } from '../components/navbar/Navbar';
+import React from "react";
+import { Navbar } from "../components/navbar/Navbar";
+import "./home.css";
 
 export const Home = () => {
   return (
     <>
-        <Navbar/>
+      <div className="vh-100 d-flex flex-column justify-content-column">
+        <header>
+          <nav>
+            <Navbar />
+          </nav>
+        </header>
+        <main className="d-flex  justify-content-center aling-items-center main border border-dark ">
+          <article className="articulo d-flex justify-content-between aling-items-center flex-column border border-dark">
+            <section className="m-auto border border-dark">
+              <h1 className="titulo border border-dark m-auto ">Arreglos Ya</h1>
+            </section>
+            <section className="">
+              <div className="d-flex justify-content-center aling-items-center border border-dark  ">
+                <form action="">
+                  <input
+                    type="search"
+                    placeholder="Que estas buscando?"
+                    className="barraNav text-center rounded form-control border border-dark  "
+                  />
+                </form>
+              </div>
+            </section>
+          </article>
+        </main>
+      </div>
     </>
-  )
-}
+  );
+};
