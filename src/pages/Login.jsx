ba2f10f00326a90b211749d9fa30a39f57043493
estebanespinola <estebanespinola65@gmail.com>
2023-06-08 16:06:20 -0300

    login modificado, botones y estilos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ export const Login = () => {
     <div className='d-flex flex-column justify-content-between border vh-100 '>
         <Navbar />
 
-        <div className='m-auto border p-5 shadow bg-body-tertiary rounded'>
+        <div className='m-auto border p-5 shadow-sm  mb-5 bg-body-tertiary rounded col-11 col-sm-8 col-lg-4'>
             <h2 className='text titulo'>Inicio de sesión</h2>
         <form className='d-flex flex-column'>    
             <label htmlFor="">Nombre de usuario</label>    
@@ -16,11 +16,14 @@ export const Login = () => {
 
             <label htmlFor="">Contraseña</label>    
             <input className='form-control' type="password" required placeholder='' />
-
-            <button className='btn btn-warning mt-3'>Iniciar sesión</button>
-            <p className='mt-2'>¿No tenes cuenta? <Link to='/profesional/registro'>Registrate</Link></p>
+            <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+            <label className="form-check-label" for="form2Example31"> Recuerdame </label>
             <Link className='d-flex justify-content-center ' to='/profesional/registro'>¿ Olvidaste tu contraseña?</Link>
 
+            </div>
+            <button className='btn btn-warning mt-3'>Iniciar sesión</button>
+            <p className='mt-2'>¿No tenes cuenta? <Link to='/profesional/registro'>Registrate</Link></p>
         </form>
         </div>
 
