4b308366acf344262ec6c9fc03787872128cd2cb
maximilianopietkiewicz <maximilianopietkiewicz04@gmail.com>
2023-06-08 16:33:44 -0300

    avanzando con la pagina de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navbar } from "../components/navbar/Navbar";
 import "./home.css";
+import { Search } from "../components/searchbar/Search";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -12,21 +14,33 @@ export const Home = () => {
           </nav>
         </header>
         <main className="d-flex  justify-content-center aling-items-center main border border-dark ">
-          <article className="articulo d-flex justify-content-between aling-items-center flex-column border border-dark">
+          <article className="articulo-1 d-flex justify-content-around aling-items-center flex-column border border-dark ">
             <section className="m-auto border border-dark">
-              <h1 className="titulo border border-dark m-auto ">Arreglos Ya</h1>
+              <h1 className="titulo  m-auto ">Arreglos Ya</h1>
             </section>
             <section className="">
               <div className="d-flex justify-content-center aling-items-center border border-dark  ">
-                <form action="">
-                  <input
-                    type="search"
-                    placeholder="Que estas buscando?"
-                    className="barraNav text-center rounded form-control border border-dark  "
-                  />
-                </form>
+                <img src="./logos/busqueda.png" alt="" className="logo" />
+                <Search />
               </div>
             </section>
+            <section className="d-flex justify-content-around aling-items-center m-auto col-10 col-sm-8 col-lg-5 border border border-dark">
+              <Link to="/" className="text-decoration-none text-dark">
+                Electricista
+              </Link>
+              <Link to="/" className="text-decoration-none text-dark">
+                Plomero
+              </Link>
+              <Link to="/" className="text-decoration-none text-dark">
+                Carpintero
+              </Link>
+              <Link to="/" className="text-decoration-none text-dark">
+                Gasista
+              </Link>
+            </section>
+          </article>
+          <article className="articulo-2 d-flex justify-content-between aling-items-center flex-column ">
+            <section></section>
           </article>
         </main>
       </div>
